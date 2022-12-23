7f95c26ecbcff1aca98880de57b7169720c0aaac
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2022-12-23 15:57:02 +0200

    New count score && with redo and undo

--- connect 4 project.c
+++ connect 4 project.c
@@ -4,26 +4,27 @@
 
 int main()
 {
-    int totMove=0, nofUndo=0;
+    int totMoves=0, nofUndo=0;
     int hight, width;
     player first, second;
-    first.symbol='X', second.symbol='O';
-    first.numOfMove=0; second.numOfMove=0;
+    first.symbol = 'X', second.symbol = 'O';
+    first.numOfMove = 0; second.numOfMove = 0;
+    first.score = 0, second.score = 0;
     scanf("%d %d", &width, &hight);
     int rows=hight, cols=width;
     int available_cols[cols], select_cols[rows*cols], undo_moves[rows*cols][2];
     char game[rows][cols];
     creat_game(rows, cols, game, available_cols, select_cols, undo_moves);
 
-    while(totMove < (rows*cols))
+    while(totMoves < (rows*cols))
     {
-        if(totMove%2 == 0) inGame_menu(rows, cols, game, undo_moves, available_cols, select_cols, &nofUndo, &totMove, first);
-        else inGame_menu(rows, cols, game, undo_moves, available_cols, select_cols, &nofUndo, &totMove, second);
+        if(totMoves%2 == 0){
+            inGame_menu(rows, cols, game, undo_moves, available_cols, select_cols, &nofUndo, &totMoves, &first, &second);
+        }else{
+            inGame_menu(rows, cols, game, undo_moves, available_cols, select_cols, &nofUndo, &totMoves, &second, &first);
+        }
+            scan_game(rows, cols, game);
+            printf("totMoves = %d\nfirst score = %d ,second score = %d\n", totMoves, first.score, second.score);
     }
-
-
- printf("%d ",CountScore(rows,cols,first,game));
- printf("%d ",CountScore(rows,cols,second,game));
-
-
+    return 0;
 }
--- funcion.h
+++ funcion.h
@@ -44,139 +44,68 @@ void creat_game(int rows, int cols, char game[][cols], int available_cols[], int
 
 void start_game(int rows, int cols, char game[][cols])
 {
-
 }
 
-
-void inGame_menu(int rows, int cols, char game[][cols], int undo_moves[][2], int available_cols[], int select_cols[], int *nofUndo, int *totMove, player playerTurn)
+void inGame_menu(int rows, int cols, char game[][cols], int undo_moves[][2], int available_cols[], int select_cols[], int *nofUndo, int *totMove, player *playerTurn, player *other)
 {
     int select_menu;
-    printf("Enter \n1 for a move \n2 for undo \n3 for redo \n4 for save \n5 for quit\n");
+    printf("Enter -1 for in Game menu\n");
     scanf("%d", &select_menu);
-    switch(select_menu)
-    {
-    case 1 :
-        move_player(rows, cols, game, available_cols, select_cols, totMove, playerTurn);
+    if(select_menu != -1){
+        make_move(rows, cols, game, available_cols, select_cols, totMove, select_menu, playerTurn);
+        playerTurn->score += count_score(rows, cols, game, available_cols, select_cols, *totMove, playerTurn);
         for(int i=0; i<*nofUndo; i++)
             for(int j=0; j<2; j++)
                 undo_moves[i][j] = 0;
         *nofUndo = 0;
-        break;
-    case 2 :
-        if(*totMove <= 0)  return inGame_menu(rows, cols, game, undo_moves, available_cols, select_cols, nofUndo, totMove, playerTurn);
-        undo(rows, cols, game, nofUndo, totMove, select_cols, available_cols, undo_moves);
-        break;
-    case 3 :
-        if(*nofUndo <= 0)   return inGame_menu(rows, cols, game, undo_moves, available_cols, select_cols, nofUndo, totMove, playerTurn);
-        redo(rows, cols, game, nofUndo, totMove, select_cols, available_cols, undo_moves, playerTurn
-             );
-        break;
-    case 4 :
-        //save
-    case 5 :
-        //quit
-        break;
-    default :
-        break;
+    }else{
+        printf("Enter \n1 for a move \n2 for undo \n3 for redo \n4 for save \n5 for quit\n");
+        scanf("%d", &select_menu);
+        switch(select_menu)
+        {
+        case 1 :
+            make_move(rows, cols, game, available_cols, select_cols, totMove, select_menu, playerTurn);
+            playerTurn->score += count_score(rows, cols, game, available_cols, select_cols, *totMove, playerTurn);
+            for(int i=0; i<*nofUndo; i++)
+                for(int j=0; j<2; j++)
+                    undo_moves[i][j] = 0;
+            *nofUndo = 0;
+            break;
+        case 2 :
+            if(*totMove <= 0)  return inGame_menu(rows, cols, game, undo_moves, available_cols, select_cols, nofUndo, totMove, playerTurn, other);
+            other->score -= count_score(rows, cols, game, available_cols, select_cols, *totMove, other);
+            undo(rows, cols, game, nofUndo, totMove, select_cols, available_cols, undo_moves);
+            break;
+        case 3 :
+            if(*nofUndo <= 0)   return inGame_menu(rows, cols, game, undo_moves, available_cols, select_cols, nofUndo, totMove, playerTurn, other);
+            redo(rows, cols, game, nofUndo, totMove, select_cols, available_cols, undo_moves, playerTurn, other);
+            playerTurn->score += count_score(rows, cols, game, available_cols, select_cols, *totMove, playerTurn);
+            break;
+        case 4 :
+            //save
+        case 5 :
+            //quit
+            break;
+        default :
+            break;
+        }
     }
 }
 
-
-int CountScore(int rows, int cols,player playerTurn,char game[rows][cols])
+void make_move(int rows, int cols, char game[][cols], int available_cols[], int select_cols[], int *totMove, int nofcol, player playerTurn)
 {
-    playerTurn.score=0;
-    int counter=0,x,y;
-
-//count score Horizontally
-    for(int i=1;i<rows;i+=2,counter=0)
-    {
-      for(int j=1;j<=cols;j+=2)
-      {
-        //if we don't want to look outside the game[][]->if(J==cols)J--
-       if((game[i][j]==playerTurn.symbol))counter++;
-        else
-        {
-        if(counter>=4)playerTurn.score+=(counter-4+1);
-        counter =0;
-        }
-      }
+    if(nofcol < 1 || nofcol > cols || available_cols[nofcol-1] == 0){
+        printf("please, Enter a vaild nof col..\n");
+        scanf("%d", &nofcol);
+        return make_move(rows, cols, game, available_cols, select_cols, totMove, nofcol, playerTurn);
+    }else{
+        game[available_cols[nofcol-1]-1][nofcol-1] = playerTurn.symbol;
+        available_cols[nofcol-1]--;
+        select_cols[*totMove] = nofcol;
+        playerTurn.numOfMove++, *totMove+=1;
     }
-//count score  vertically
-     for(int j=1;j<cols;j+=2,counter=0)
-    {
-      for(int i=1;i<=rows;i+=2)
-      {
-        //if we don't want to look outside the game[][]->if(i==rows)i--
-       if((game[i][j]==playerTurn.symbol))counter++;
-        else
-        {
-        if(counter>=4)playerTurn.score+=(counter-4+1);
-        counter =0;
-        }
-      }
-    }
-
-//count score diagonally
-    for (int i = 1; i <rows; i+=2,counter=0)
-    {x=i;
-        for (int j=1; j <cols; j+=2)
-        {y=j;
-            while(y<=cols&&x<=rows)
-            {
-                //if we don't want to look outside the game[][]->if(x==rows||y==cols)->x--;y--;
-              if(game[x][y]==playerTurn.symbol)counter++;
-              else
-              {
-              if(counter>=4)playerTurn.score+=(counter-4+1);
-              counter=0;
-              }
-              x+=2;y+=2;
-            }
-            x=i;
-            if (i>1)break;
-        }
-    }
-
-//count score diagonal backward
-    for (int i = 1; i <rows; i+=2,counter=0)
-    {x=i;
-
-        for (int j=(cols-2);j>0;j-=2)
-        {y=j;
-            while(y>=-1&&x<=rows)
-            {
-                 //if we don't want to look outside the game[][]->if(x==rows||y==-1)->x--;y++
-              if(game[x][y]==playerTurn.symbol)
-              counter++;
-              else
-              {
-              if(counter>=4)playerTurn.score+=(counter-4+1);
-              counter=0;
-              }
-              x+=2;y-=2;
-            }
-            x=i;
-            if (i>1)break;
-        }
-
-    }
-    return playerTurn.score;
-}
-
-
-void move_player(int rows, int cols, char game[][cols], int available_cols[], int select_cols[], int *totMove, player playerTurn)
-{
-    int nofcol;
-    printf("select the number of col\n");
-    scanf("%d", &nofcol);
-    game[available_cols[nofcol-1]-1][nofcol-1] = playerTurn.symbol;
-    available_cols[nofcol-1]--;
-    scan_game(rows, cols, game);
-    select_cols[*totMove] = nofcol;
-    playerTurn.numOfMove++, *totMove+=1;
     return 0;
 }
-
 
 void undo(int rows, int cols, char game[][cols], int *nofUndo, int *totMove, int select_cols[], int available_cols[], int undo_moves[][2])
 {
@@ -184,7 +113,6 @@ void undo(int rows, int cols, char game[][cols], int *nofUndo, int *totMove, int
     des_col = select_cols[*totMove-1] - 1;
     des_row = available_cols[des_col] ;
     game[des_row][des_col] = ' ';
-    scan_game(rows, cols, game);
     undo_moves[*nofUndo][0] = des_row;
     undo_moves[*nofUndo][1] = des_col;
     select_cols[*totMove-1] = 0;            // -> minus values by 1 && increas nofUndo by 1
@@ -193,17 +121,46 @@ void undo(int rows, int cols, char game[][cols], int *nofUndo, int *totMove, int
     return 0;
 }
 
-void redo(int rows, int cols, char game[][cols], int *nofUndo, int *totMove, int select_cols[], int available_cols[], int undo_moves[][2], player playerTurn)
+void redo(int rows, int cols, char game[][cols], int *nofUndo, int *totMove, int select_cols[], int available_cols[], int undo_moves[][2], player *playerTurn)
 {
     int des_row, des_col;
-    char symbol;
-    if(*totMove%2 == 0) symbol = playerTurn.symbol;
-    else symbol = playerTurn.symbol;
     des_row = undo_moves[*nofUndo-1][0];
     des_col = undo_moves[*nofUndo-1][1];
-    game[des_row][des_col] = symbol;
-    scan_game(rows, cols, game);
+    game[des_row][des_col] = playerTurn->symbol;
+    undo_moves[*nofUndo-1][0] = 0;
+    undo_moves[*nofUndo-1][1] = 0;
     *totMove+=1, *nofUndo-=1;
-    select_cols[*totMove-1] = des_col;
+    select_cols[*totMove-1] = des_col+1;
     available_cols[des_col]--;
+}
+
+int count_4(int rows, int cols, char game[][cols], int des_row, int des_col, player playerTurn, int i, int j)
+{
+    if((des_row + i) < 0 || (des_row + i) > (rows-1) || (des_col + j) < 0 || (des_col + j) > (cols-1)) return 0;
+    int counter=0, score=0;
+    for(int ii=0; ii < 4; ii++)
+    {
+        counter += (int)game[des_row + i][des_col + j];
+        if(i < 0) i++;
+        else if(i > 0) i--;
+        if(j < 0) j++;
+        else if(j > 0) j--;
+    }
+    if(counter == 4*(int)playerTurn.symbol) score++;
+    return score;
+}
+
+int count_score(int rows, int cols, char game[][cols], int available_cols[], int select_cols[], int totMoves, player playerTurn)
+{
+    int score=0;
+    int des_col = select_cols[totMoves-1] - 1;
+    int des_row = available_cols[des_col];
+    score += count_4(rows, cols, game, des_row, des_col, playerTurn, 0, 3);
+    score += count_4(rows, cols, game, des_row, des_col, playerTurn, 0, -3);
+    score += count_4(rows, cols, game, des_row, des_col, playerTurn, 3, 0);  //->all possiple dimension for connect4
+    score += count_4(rows, cols, game, des_row, des_col, playerTurn, 3, 3);
+    score += count_4(rows, cols, game, des_row, des_col, playerTurn, -3, -3);
+    score += count_4(rows, cols, game, des_row, des_col, playerTurn, -3, 3);
+    score += count_4(rows, cols, game, des_row, des_col, playerTurn, 3, -3);
+    return score;
 }
